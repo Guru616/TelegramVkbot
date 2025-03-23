573ed03a591fc79da06138384db48d1233a9ce7c
Guru <Guru@gmail.com>
2025-03-23 23:21:15 +0900

    master

---
+++ config.py
@@ -0,0 +1,11 @@
+TG_API_TOKEN = ''
+VK_SERVIS_KEY = ''
+#NAMES_GROUP_LIST = 'dromchita','drom.chita75'
+NAMES_GROUP_LIST = (
+    'dromchita',
+    'drom.chita75',
+    'dromavt',
+    'drom75ru',
+    'chita.drom')
+CHANNEL_ID = '-1002175631698'
+ID_CHAT_BOT = 1467678672
